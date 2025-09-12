edf44c691d61234b84c4410c7db4f92da3371818
IMjaeyongpark <jaeyong36@naver.com>
2025-09-12 15:49:04 +0900

    프로젝트 설정

---
+++ src/main/java/JYBank/JYBank/JyBankApplication.java
@@ -0,0 +1,13 @@
+package JYBank.JYBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JyBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JyBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/JYBank/JYBank/JyBankApplicationTests.java
@@ -0,0 +1,13 @@
+package JYBank.JYBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JyBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
